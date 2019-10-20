85f51c9ad49a7ad1da2ebf14c3f376edf86b30a5
NelsonCad <NelsonCad97@gmail.com>
2019-10-20 16:08:36 -0700

    added spacing and styling from Project three to test functionality, styling will change later

--- src/components/project-card/index.js
+++ src/components/project-card/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Card, Button, CardTitle, CardText } from "reactstrap";
+import "./project-card.css";
 
 function Project(props) {
 
     return (
-        <Card body id={props.id}>
-            <CardTitle>{props.title}</CardTitle>
-            <CardText>{props.description}</CardText>
-            <Button href={props.link} target="_blank">go to website</Button>
-        </Card>
+            <Card body id={props.id} className="item-display-wrap">
+                <CardTitle>{props.title}</CardTitle>
+                <CardText>{props.description}</CardText>
+                <Button href={props.link} target="_blank">go to website</Button>
+            </Card>
     );
 };
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,32 +1,15 @@
 import React from "react";
 import Jumbo from "../components/Jumbotron";
 import Project from "../components/project-card";
-
-const projects = [{
-    id: 0,
-    title: "Garage Trader",
-    description: "like Ebay, but without money",
-    link: "https://protected-shelf-46012.herokuapp.com"
-},
-{
-    id: 1,
-    title: "Liri-Node-Search",
-    description: "node application with various APIs",
-    link: "https://github.com/NelsonCad/Liri-node-search"
-},
-{
-    id: 2,
-    title: "Mongoose Scraper",
-    description: "iflscience.com scraper",
-    link: "https://dry-anchorage-82464.herokuapp.com"
-}]
+import projects from "../components/projectList";
+import "./portfolio.css"
 
 function Portfolio() {
 
     return (
         <div>
             <Jumbo>
-            <div className="item-display-container">
+            <div className="item-display-container cardBuild">
             {projects.map(project => (
                 <Project 
                 id={project.id}
