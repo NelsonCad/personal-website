e11fa6c869b6c80888504ea26a8e2421df655072
NelsonCad <NelsonCad97@gmail.com>
2019-10-24 16:59:25 -0700

    filled out the contact page, grammar fixes in the home page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Space from "../components/Emptyspace";
 import "./placeholder.css";
+import {
+    Container,
+    Row,
+    Col
+} from "reactstrap";
 
 function Contact () {
 
@@ -8,7 +13,26 @@ function Contact () {
     return (
         <div>
             <Space />
-            <p>this is the Contact page</p>
+            <Container className="container">
+                <h1>How to contact me</h1>
+                <hr></hr>
+
+                <Row className="row">
+
+                    <Col sm="6">
+                        <p>Email: NelonCad97@gmail.com</p>
+                        <p>Phone: (602) 448-6900</p>
+                    </Col>
+
+                    <Col sm="6">
+                        <p>Address: 549 E Benrich Dr.</p>
+                        <p>Gilbert, AZ 85295</p>
+                        
+                    </Col>
+
+                </Row>
+
+            </Container>
         </div>
     );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -24,11 +24,11 @@ function Home() {
 
                     <Col sm="10">
                         <p>
-                            I am a creative Web Developer who wishes to combine artful functionality with user-friendly experiences. I work to make every thing that i create to feel unique and meaningful, from the websites that i will make and currently making to (eventually) the vidoegames that will come to life.
+                            I am a creative Web Developer who wishes to combine artful functionality with user-friendly experiences. I work to make every thing that I create to feel unique and meaningful, from the websites that I will make and currently making to (eventually) the vidoegames that will come to life.
                         </p>
 
                         <p>
-                            On the 30th of September, 2019, I completed the intensive 12 week Bootcamp and have received my certificate for Full Stack Web Development using Javascript as the core coding language, and have learned to use HTML, CSS, JQuery, Reactjs (which is being used to create this website), MySQL, MongoDB, Node.js, and various npm packages such as Sequalize, Mongoose, Express, and many more. Of course, there is far more to learn about in JavaScript and of what others have done in it, and i am more than happy to learn as much as possible about it. but why stop at JavaScript?
+                            On the 30th of September, 2019, I completed the intensive 12 week Bootcamp and have received my certificate for Full Stack Web Development using Javascript as the core coding language, and have learned to use HTML, CSS, JQuery, Reactjs (which is being used to create this website), MySQL, MongoDB, Node.js, and various npm packages such as Sequalize, Mongoose, Express, and many more. Of course, there is far more to learn about in JavaScript and of what others have done in it, and I am more than happy to learn as much as possible about it.
                         </p>
 
                     </Col>
