feefa86608e3a30046e7a0ce2a328297eb6eda60
NelsonCad <NelsonCad97@gmail.com>
2019-10-23 18:00:09 -0700

    added structure to the Home page, and a bit about myself to fill it with content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,42 @@
 import React from "react";
 import Space from "../components/Emptyspace";
 import "./placeholder.css";
+import {
+    Container,
+    Row,
+    Col
+} from "reactstrap";
 
 function Home() {
-
 
     return (
         <div>
             <Space />
-            <p>this is the Home page</p>
+            <Container className="container">
+                <h1>About Me</h1>
+                <hr></hr>
+
+                <Row className="row">
+
+                    <Col sm="2">
+                        <h3>Who am I?</h3>
+                    </Col>
+
+                    <Col sm="10">
+                        <p>
+                            I am a creative Web Developer who wishes to combine artful functionality with user-friendly experiences. I work to make every thing that i create to feel unique and meaningful, from the websites that i will make and currently making to (eventually) the vidoegames that will come to life.
+                        </p>
+
+                        <p>
+                            On the 30th of September, 2019, I completed the intensive 12 week Bootcamp and have received my certificate for Full Stack Web Development using Javascript as the core coding language, and have learned to use HTML, CSS, JQuery, Reactjs (which is being used to create this website), MySQL, MongoDB, Node.js, and various npm packages such as Sequalize, Mongoose, Express, and many more. Of course, there is far more to learn about in JavaScript and of what others have done in it, and i am more than happy to learn as much as possible about it. but why stop at JavaScript?
+                        </p>
+
+                    </Col>
+
+                </Row>
+
+            </Container>
+            
         </div>
     );
 };
